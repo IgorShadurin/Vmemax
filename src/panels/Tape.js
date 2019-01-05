c1f8adc1ed5d019c47c45cb790d792401d7fbd39
igar1991 <iiggaarr19912004@gmail.com>
2019-01-05 09:59:42 +0300

    first commit

--- src/panels/Tape.js
+++ src/panels/Tape.js
@@ -5,6 +5,102 @@ import picter2 from '../img/2.jpg';
 import picter3 from '../img/3.jpg';
 import picter4 from '../img/4.jpg';
 import picter5 from '../img/5.jpg';
+import picter6 from '../img/6.jpg';
+import picter7 from '../img/7.jpg';
+import picter8 from '../img/8.jpg';
+import picter9 from '../img/9.jpg';
+import picter10 from '../img/10.jpg';
+import picter11 from '../img/11.jpg';
+import picter12 from '../img/12.jpg';
+import picter13 from '../img/13.jpg';
+import picter14 from '../img/14.jpg';
+import picter15 from '../img/15.jpg';
+import picter16 from '../img/16.jpg';
+import picter17 from '../img/17.jpg';
+import picter18 from '../img/18.jpg';
+import picter19 from '../img/19.jpg';
+import picter20 from '../img/20.jpg';
+import picter21 from '../img/21.jpg';
+import picter22 from '../img/22.jpg';
+import picter23 from '../img/23.jpg';
+import picter24 from '../img/24.jpg';
+import picter25 from '../img/25.jpg';
+import picter26 from '../img/26.jpg';
+import picter27 from '../img/27.jpg';
+import picter28 from '../img/28.jpg';
+import picter29 from '../img/29.jpg';
+import picter30 from '../img/30.jpg';
+import picter31 from '../img/31.jpg';
+import picter32 from '../img/32.jpg';
+import picter33 from '../img/33.jpg';
+import picter34 from '../img/34.jpg';
+import picter35 from '../img/35.jpg';
+import picter36 from '../img/36.jpg';
+import picter37 from '../img/37.jpg';
+import picter38 from '../img/38.jpg';
+import picter39 from '../img/39.jpg';
+import picter40 from '../img/40.jpg';
+import picter41 from '../img/41.jpg';
+import picter42 from '../img/42.jpg';
+import picter43 from '../img/43.jpg';
+import picter44 from '../img/44.jpg';
+import picter45 from '../img/45.jpg';
+import picter46 from '../img/46.jpg';
+import picter47 from '../img/47.jpg';
+import picter48 from '../img/48.jpg';
+import picter49 from '../img/49.jpg';
+import picter50 from '../img/50.jpg';
+import picter51 from '../img/51.jpg';
+import picter52 from '../img/52.jpg';
+import picter53 from '../img/53.jpg';
+import picter54 from '../img/54.jpg';
+import picter55 from '../img/55.jpg';
+import picter56 from '../img/56.jpg';
+import picter57 from '../img/57.jpg';
+import picter58 from '../img/58.jpg';
+import picter59 from '../img/59.jpg';
+import picter60 from '../img/60.jpg';
+import picter61 from '../img/61.jpg';
+import picter62 from '../img/62.jpg';
+import picter63 from '../img/63.jpg';
+import picter64 from '../img/64.jpg';
+import picter65 from '../img/65.jpg';
+import picter66 from '../img/66.jpg';
+import picter67 from '../img/67.jpg';
+import picter68 from '../img/68.jpg';
+import picter69 from '../img/69.jpg';
+import picter70 from '../img/70.jpg';
+import picter71 from '../img/71.jpg';
+import picter72 from '../img/72.jpg';
+import picter73 from '../img/73.jpg';
+import picter74 from '../img/74.jpg';
+import picter75 from '../img/75.jpg';
+import picter76 from '../img/76.jpg';
+import picter77 from '../img/77.jpg';
+import picter78 from '../img/78.jpg';
+import picter79 from '../img/79.jpg';
+import picter80 from '../img/80.jpg';
+import picter81 from '../img/81.jpg';
+import picter82 from '../img/82.jpg';
+import picter83 from '../img/83.jpg';
+import picter84 from '../img/84.jpg';
+import picter85 from '../img/85.jpg';
+import picter86 from '../img/86.jpg';
+import picter87 from '../img/87.jpg';
+import picter88 from '../img/88.jpg';
+import picter89 from '../img/89.jpg';
+import picter90 from '../img/90.jpg';
+import picter91 from '../img/91.jpg';
+import picter92 from '../img/92.jpg';
+import picter93 from '../img/93.jpg';
+import picter94 from '../img/94.jpg';
+import picter95 from '../img/95.jpg';
+import picter96 from '../img/96.jpg';
+import picter97 from '../img/97.jpg';
+import picter98 from '../img/98.jpg';
+import picter99 from '../img/99.jpg';
+import picter100 from '../img/100.jpg';
+
 import Gallery from 'react-photo-gallery';
 
 const PHOTO_SET = [
@@ -30,7 +126,482 @@ const PHOTO_SET = [
   },
   {
     src: picter5,
+    width: 1,
+    height: 1
+  },
+  {
+    src: picter6,
+    width: 3,
+    height: 2
+  },
+  {
+    src: picter7,
     width: 2,
+    height: 1
+  },
+  {
+    src: picter8,
+    width: 1,
+    height: 1
+  },
+  {
+    src: picter9,
+    width: 3,
+    height: 2
+  },
+  {
+    src: picter10,
+    width: 2,
+    height: 1
+  },
+  {
+    src: picter11,
+    width: 1,
+    height: 1
+  },
+  {
+    src: picter12,
+    width: 3,
+    height: 2
+  },
+  {
+    src: picter13,
+    width: 1,
+    height: 1
+  },
+  {
+    src: picter14,
+    width: 1,
+    height: 1
+  },
+  {
+    src: picter15,
+    width: 2,
+    height: 1
+  },
+  {
+    src: picter16,
+    width: 1,
+    height: 1
+  },
+  {
+    src: picter17,
+    width: 1,
+    height: 1
+  },
+  {
+    src: picter18,
+    width: 1,
+    height: 1
+  },
+  {
+    src: picter19,
+    width: 3,
+    height: 2
+  },
+  {
+    src: picter20,
+    width: 1,
+    height: 1
+  },
+  {
+    src: picter21,
+    width: 1,
+    height: 1
+  },
+  {
+    src: picter22,
+    width: 1,
+    height: 1
+  },
+  {
+    src: picter23,
+    width: 1,
+    height: 1
+  },
+  {
+    src: picter24,
+    width: 1,
+    height: 1
+  },
+  {
+    src: picter25,
+    width: 3,
+    height: 2
+  },
+  {
+    src: picter26,
+    width: 1,
+    height: 1
+  },
+  {
+    src: picter27,
+    width: 3,
+    height: 2
+  },
+  {
+    src: picter28,
+    width: 3,
+    height: 2
+  },
+  {
+    src: picter29,
+    width: 3,
+    height: 2
+  },
+  {
+    src: picter30,
+    width: 3,
+    height: 2
+  },
+  {
+    src: picter31,
+    width: 1,
+    height: 1
+  },
+  {
+    src: picter32,
+    width: 3,
+    height: 2
+  },
+  {
+    src: picter33,
+    width: 1,
+    height: 1
+  },
+  {
+    src: picter34,
+    width: 1,
+    height: 1
+  },
+  {
+    src: picter35,
+    width: 2,
+    height: 1
+  },
+  {
+    src: picter36,
+    width: 3,
+    height: 2
+  },
+  {
+    src: picter37,
+    width: 3,
+    height: 2
+  },
+  {
+    src: picter38,
+    width: 1,
+    height: 1
+  },
+  {
+    src: picter39,
+    width: 1,
+    height: 1
+  },
+  {
+    src: picter40,
+    width: 3,
+    height: 2
+  },
+  {
+    src: picter41,
+    width: 3,
+    height: 2
+  },
+  {
+    src: picter42,
+    width: 3,
+    height: 2
+  },
+  {
+    src: picter43,
+    width: 1,
+    height: 1
+  },
+  {
+    src: picter44,
+    width: 1,
+    height: 1
+  },
+  {
+    src: picter45,
+    width: 3,
+    height: 2
+  },
+  {
+    src: picter46,
+    width: 1,
+    height: 1
+  },
+  {
+    src: picter47,
+    width: 1,
+    height: 1
+  },
+  {
+    src: picter48,
+    width: 1,
+    height: 1
+  },
+  {
+    src: picter49,
+    width: 1,
+    height: 1
+  },
+  {
+    src: picter50,
+    width: 1,
+    height: 1
+  },
+  {
+    src: picter51,
+    width: 1,
+    height: 1
+  },
+  {
+    src: picter52,
+    width: 1,
+    height: 1
+  },
+  {
+    src: picter53,
+    width: 1,
+    height: 1
+  },
+  {
+    src: picter54,
+    width: 1,
+    height: 1
+  },
+  {
+    src: picter55,
+    width: 1,
+    height: 1
+  },
+  {
+    src: picter56,
+    width: 1,
+    height: 1
+  },
+  {
+    src: picter57,
+    width: 1,
+    height: 1
+  },
+  {
+    src: picter58,
+    width: 1,
+    height: 1
+  },
+  {
+    src: picter59,
+    width: 1,
+    height: 1
+  },
+  {
+    src: picter60,
+    width: 1,
+    height: 1
+  },
+  {
+    src: picter61,
+    width: 1,
+    height: 1
+  },
+  {
+    src: picter62,
+    width: 1,
+    height: 1
+  },
+  {
+    src: picter63,
+    width: 1,
+    height: 1
+  },
+  {
+    src: picter64,
+    width: 1,
+    height: 1
+  },
+  {
+    src: picter65,
+    width: 1,
+    height: 1
+  },
+  {
+    src: picter66,
+    width: 1,
+    height: 1
+  },
+  {
+    src: picter67,
+    width: 1,
+    height: 1
+  },
+  {
+    src: picter68,
+    width: 1,
+    height: 1
+  },
+  {
+    src: picter69,
+    width: 1,
+    height: 1
+  },
+  {
+    src: picter70,
+    width: 1,
+    height: 1
+  },
+  {
+    src: picter71,
+    width: 1,
+    height: 1
+  },
+  {
+    src: picter72,
+    width: 1,
+    height: 1
+  },
+  {
+    src: picter73,
+    width: 1,
+    height: 1
+  },
+  {
+    src: picter74,
+    width: 1,
+    height: 1
+  },
+  {
+    src: picter75,
+    width: 1,
+    height: 1
+  },
+  {
+    src: picter76,
+    width: 1,
+    height: 1
+  },
+  {
+    src: picter77,
+    width: 1,
+    height: 1
+  },
+  {
+    src: picter78,
+    width: 1,
+    height: 1
+  },
+  {
+    src: picter79,
+    width: 1,
+    height: 1
+  },
+  {
+    src: picter80,
+    width: 1,
+    height: 1
+  },
+  {
+    src: picter81,
+    width: 1,
+    height: 1
+  },
+  {
+    src: picter82,
+    width: 1,
+    height: 1
+  },
+  {
+    src: picter83,
+    width: 1,
+    height: 1
+  },
+  {
+    src: picter84,
+    width: 1,
+    height: 1
+  },
+  {
+    src: picter85,
+    width: 1,
+    height: 1
+  },
+  {
+    src: picter86,
+    width: 1,
+    height: 1
+  },
+  {
+    src: picter87,
+    width: 1,
+    height: 1
+  },
+  {
+    src: picter88,
+    width: 1,
+    height: 1
+  },
+  {
+    src: picter89,
+    width: 1,
+    height: 1
+  },
+  {
+    src: picter90,
+    width: 1,
+    height: 1
+  },
+  {
+    src: picter91,
+    width: 1,
+    height: 1
+  },
+  {
+    src: picter92,
+    width: 1,
+    height: 1
+  },
+  {
+    src: picter93,
+    width: 1,
+    height: 1
+  },
+  {
+    src: picter94,
+    width: 1,
+    height: 1
+  },
+  {
+    src: picter95,
+    width: 1,
+    height: 1
+  },
+  {
+    src: picter96,
+    width: 1,
+    height: 1
+  },
+  {
+    src: picter97,
+    width: 1,
+    height: 1
+  },
+  {
+    src: picter98,
+    width: 1,
+    height: 1
+  },
+  {
+    src: picter99,
+    width: 1,
+    height: 1
+  },
+  {
+    src: picter100,
+    width: 1,
     height: 1
   }
 ];
@@ -38,7 +609,7 @@ class Tape extends React.Component {
 	constructor(props){
 		super(props);
 			this.state = {			
-		
+		columns: 3
 	}
 }
 drawThisImage(image) {
@@ -55,7 +626,7 @@ return (
 		
 			<Div>
 				
-				<Gallery photos={PHOTO_SET} onClick = {(index)=>{this.drawThisImage(index.target.src)}}/>
+				<Gallery photos={PHOTO_SET} onClick = {(index)=>{this.drawThisImage(index.target.src)}} columns = {this.state.columns}/>
 			</Div>
 		
 	</Panel>
